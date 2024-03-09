003362ba4fa962e555562f7d0fc4c2a8a9d2c7cd
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-09 17:53:49 +0100

    :art: function toggle  :art:

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import {useRef} from "react";
 import "./Navbar.css";
 import { FaBars, FaTimes } from "react-icons/fa";
 
+
 export const Navbar = () => {
+
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    };
+
   return (
     <header>
-      <h3>Logo</h3>
-      <nav>
-        <a href="/#">Homme</a>
-        <a href="/#">My work</a>
-        <a href="/#">Blog</a>
-        <a href="/#">About me</a>
-      </nav>
-      <button>
-
-      </button>
+        <h3>Logo</h3>
+        <nav>
+            <a href="/#">Homme</a>
+            <a href="/#">My work</a>
+            <a href="/#">Blog</a>
+            <a href="/#">About me</a>
+            <button onClick={showNavbar}>
+                <FaTimes/>
+            </button>
+        </nav>
+        <button>
+            <FaBars/>
+        </button>
     </header>
   );
 };
