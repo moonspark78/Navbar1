e46b514164616ff47951ffe605bc833186d2bb2d
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-09 16:35:38 +0100

    :art: debut  :art:

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <h3>Logo</h3>
+      <nav>
+        <a href="/#">Homme</a>
+        <a href="/#">My work</a>
+        <a href="/#">Blog</a>
+        <a href="/#">About me</a>
+      </nav>
+      <button>
+
+      </button>
+    </header>
+  );
+};
