32ad22d9c5b08d6a45481d793bec9b56252191c8
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-09 22:51:54 +0100

    :art: nav  :art:

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,15 +15,15 @@ export const Navbar = () => {
     <header>
         <h3>Logo</h3>
         <nav>
-            <a href="/#">Homme</a>
+            <a href="/#">Home</a>
             <a href="/#">My work</a>
             <a href="/#">Blog</a>
             <a href="/#">About me</a>
-            <button onClick={showNavbar}>
+            <button className="nav-btn nav-close" onClick={showNavbar}>
                 <FaTimes/>
             </button>
         </nav>
-        <button>
+        <button className="nav-btn" onClick={showNavbar}>
             <FaBars/>
         </button>
     </header>
