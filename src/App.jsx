0f5bc3c06f6664a8e8031b808b0c45d215957fdf
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-03-10 17:34:45 +0100

    :art: nav  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { Back } from './Components/Back';
 import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Back/>
     </div>
   );
 }
---
+++ src/Components/Back.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import img1 from "../images/image.png"
+import "./bStyle.css"
+
+
+
+export const Back = () => {
+  return (
+    <div>
+        <img className='background' src={img1} alt='to'/>
+    </div>
+  )
+}
+
+
